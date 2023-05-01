2b142eea1069a9cefc6f67e8fb4c692f31288210
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-05-01 00:32:35 -0600

    hello world

---
+++ fundamentals/introduction/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+name = "Chelsey"
+print("Hello World", name)
+print("Hello World " +  name)
+
+num = 17
+print("Hello World", num)
+print("Hello World " + str(num))
+
+food_one = "crab"
+food_two = "pizza"
+print('I love to eat {} and {}'.format(food_one, food_two))
+print(f'I love to eat {food_one} and {food_two}.')

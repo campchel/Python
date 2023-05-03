66bf0152e7b2695482ad99b3a76af7a69e5f8445
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-05-02 23:31:39 -0600

    chaining methods

--- fundamentals/fundamentals/chaining_methods.py
+++ fundamentals/fundamentals/chaining_methods.py
@@ -1,84 +1,51 @@
-# Assignment: User
-# For this assignment you will create the user class and add a couple methods!
-
-
 class User:
 
-    def __init__(self, first_name, last_name, email, age):
-
+    def __init__(self, first_name, last_name, email, age, gender, marital_status):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
+        self.gender = gender
+        self.marital_status = marital_status
 
-    # Methods:
-    # display_info(self) - Have this method print all 
-    # of the users' details on separate lines.
-
-    def display_info(self):
-        print("==========================")
+    def display(self):
         print(f"First name: {self.first_name}")
         print(f"Last name: {self.last_name}")
         print(f"Email: {self.email}")
         print(f"Age: {self.age}")
         print(f"Member: {self.is_rewards_member}")
-        print(f"Current Points: {self.gold_card_points}")
-        print("==========================")
+        print(f"Points: {self.gold_card_points}")
+        print(f"Gender: {self.gender}")
+        print(f"Marital Status: {self.marital_status}")
 
     def enroll(self):
-
-        # NINJA BONUS
-        # Add logic in the enroll method to check if 
-        # they are a member already, and if they are, 
-        # print "User already a member." and return False, otherwise return True.
-        if(self.is_rewards_member):
-            print("User already a member.")
-            return self
-        
-        # Have this method change the user's member
-        # status to True and 
+        if self.is_rewards_member:
+            print("user is enrolled as a member")
+            return False
         self.is_rewards_member = True
 
-        # set their gold card points to 200.
+
+
+    def points(self,amount):
         self.gold_card_points = 200
 
-        return self
-
-    
-    
-    def spend_points(self, amount):
-
-        # Add logic in the spend points method
-        # to be sure they have enough points to 
-        # spend that amount and handle appropriately
+    def spend(self, amount):
         if self.gold_card_points < amount:
-            print("Insufficient points.")
-            return False
-
-        # decrease the user's points by the amount specified.
-        self.gold_card_points -= amount
+            return "Not enought points"
+        
+        self.gold_card_points = self.gold_card_points - amount
 
 
+user1 = User("CaleeAnn", "Wrath", "caleeannwrath@gmail.com", 38, "Female", "Married")
+user1.display().enroll().display().points(200).display().spend(50).display()
 
-my_user = User("Sadie", "Flick", "sflick@codingdojo.com", 39)
-my_user.display_info()
-
-user2 = User("Clifford", "Brown", "cbrown@codingdojo.com", 25)
-user3 = User("Sonny", "Rollins", "srollins@codingdojo.com", 92)
-
-my_user.spend_points(50)
-user2.enroll().spend_points(80)
-
-my_user.display_info()
-user2.display_info()
-user3.display_info()
+user2 = User("Chelsey", "Campbell", "cc.chels.campbell@gmail.com", 38, "Female", "Single")
+user2.display().enroll().display().points(200).display().spend(80).display()
 
 
-# Terminal Output
-## my_user information prints
-## Insufficient points
-## Prints my_user info again (doesn't change)
-## User2 info -- 120 gold points, mem == True
-## Show user 3 info, hasn't changed
+
+
+
+
